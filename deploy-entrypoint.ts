a4c66d02c5171fa7fa9381a1d371bb714e586765
outcastgeek <outcastgeek+gitkraken@gmail.com>
2026-08-08 13:29:07 -0400

    Use the first-party Fresh deployment preset
    Co-authored-by: outcastgeek <outcastgeek+gitkraken@gmail.com>
    Signed-off-by: outcastgeek <outcastgeek+gitkraken@gmail.com>

--- deploy-entrypoint.ts
+++
@@ -1,9 +0,0 @@
-import freshServer from "./_fresh/server.js";
-
-Deno.serve(async (request) => {
-  const response = await freshServer.fetch(request);
-  console.log(
-    `${request.method} ${new URL(request.url).pathname} ${response.status}`,
-  );
-  return response;
-});
